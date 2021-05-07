f46363e14545e4d5b95290604f7c1dd6a40f4a3e
Kevin <01rayo2015@gmail.com>
2021-05-07 11:21:00 -0700

    Updates

---
+++ precache-manifest.23e8abf64f2954beca64cbfa2d52ae31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd498d0f9025cd96cef51bfcebd939b4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b2b3fd117174cbf5bd21",
+    "url": "/react-portfolio/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "7af96effc6d9b4599472",
+    "url": "/react-portfolio/static/css/main.2fee053b.chunk.css"
+  },
+  {
+    "revision": "b2b3fd117174cbf5bd21",
+    "url": "/react-portfolio/static/js/2.afb81618.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/react-portfolio/static/js/2.afb81618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809521a554c2e51a2505",
+    "url": "/react-portfolio/static/js/3.2fbc3c68.chunk.js"
+  },
+  {
+    "revision": "7af96effc6d9b4599472",
+    "url": "/react-portfolio/static/js/main.08705d09.chunk.js"
+  },
+  {
+    "revision": "3fdc1b9ef459b60655ce",
+    "url": "/react-portfolio/static/js/runtime-main.788cc144.js"
+  },
+  {
+    "revision": "1f579fabf02e6d2e1538235c9ff821ba",
+    "url": "/react-portfolio/static/media/IMG_7416.1f579fab.jpeg"
+  }
+]);
