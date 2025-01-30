31eb3ec48bfac8eb8f961753db137d24104fb151
Kevin <01rayo2015@gmail.com>
2025-01-29 19:48:50 -0700

    update

--- src/Contact.js
+++ src/Contact.js
@@ -4,10 +4,10 @@ function Contact() {
     return (
         <div>
             <h1>HIRE ME</h1>
-            <a href="https://github.com/007krm" target="_blank">
+            <a href="https://github.com/007krm" target="https://github.com/007krm">
             <h2>Github Profile</h2>{" "}
           </a>
-          <a href="https://www.linkedin.com/in/kevin-rayo-meza-15994a206/" target="_blank">
+          <a href="https://www.linkedin.com/in/kevin-rayo-meza-15994a206/" target="https://www.linkedin.com/in/kevin-rayo-meza-15994a206/">
           <h2>linked in</h2>
         </a>
 
